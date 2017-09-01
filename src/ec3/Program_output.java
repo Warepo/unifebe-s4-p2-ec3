1e7561c1a5b612e4bf78fba134d0d3df8e909ae2
Anologicon <pauloraimundi2009@unifebe.edu.br>
2017-08-31 21:24:38 -0300

    out_put

--- src/ec3/Program_output.java
+++ src/ec3/Program_output.java
@@ -1,5 +1,8 @@
 package ec3;
 
+import ec3.produtos.*;
+
+import javax.swing.*;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.io.IOException;
@@ -8,10 +11,51 @@ public class Program_output
 {
 	public static void main(String[] args) throws Exception 
 	{
+		String entrada;
+		int escolha;
+
 		FileInputStream local = new FileInputStream(".\\files\\produtos.dat");
 		ObjectInputStream ler = new ObjectInputStream(local);
 
+		do {
+			entrada = JOptionPane.showInputDialog(""
+					+ "Abra os produtos que você quer:\n"
+					+ "\t1) Almofada \n"
+					+ "\t2) Caixa de som \n"
+					+ "\t3) Cerveja \n"
+					+ "\t4) Colher \n"
+					+ "\t5) Produto \n"
+					+ "\t6) Teclado \n"
+					+ "\t0) Sair"
+			);
 
+			escolha = Integer.parseInt(entrada);
+			switch (escolha)
+			{
+				case 1:
+							Almofada al = (Almofada) ler.readObject();
+							al.print_dados();
+					break;
+
+				case 2:
+
+					break;
+
+				case 3:
+
+					break;
+
+				case 4:
+
+					break;
+
+				case 5:
+
+					break;
+			}
+
+
+		} while (escolha != 0);
 
 		ler.close();
 	}
